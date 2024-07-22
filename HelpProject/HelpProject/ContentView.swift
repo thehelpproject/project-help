1f7a647107c5cbd1f429733077ca4c291372d287
Rahul Pahuja <rahulpahuja2015@gmail.com>
2024-07-22 20:33:22 +0530

    Added Swiftui project

---
+++ HelpProject/HelpProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HelpProject
+//
+//  Created by Rahul Pahuja on 22/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HelpProject/HelpProject/HelpProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelpProjectApp.swift
+//  HelpProject
+//
+//  Created by Rahul Pahuja on 22/07/24.
+//
+
+import SwiftUI
+
+@main
+struct HelpProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
