43d12b810c08b52d1b8cf11a41ea5dd787387a86
Rahul Pahuja <rahulpahuja2015@gmail.com>
2024-07-28 22:58:17 +0530

    Extracted Properties

--- HelpProject/HelpProject/HTextField.swift
+++ HelpProject/HelpProject/HTextField.swift
@@ -10,6 +10,18 @@ import SwiftUI
 struct HTextField: View {
     @State private var statex:String
     @State private var title:String
+    @State private var startImage:String = "network"
+    @State private var endImage:String = "x.circle"
+    @State private var EMPTY_STRING:String = ""
+    @State private var lineWidth:CGFloat = 2
+    @State private var trailingInset:CGFloat = 0
+    @State private var leadingInset:CGFloat = 0
+    @State private var bottomInset:CGFloat  = 4
+    @State private var topInset:CGFloat = 4
+    @State private var shadowRadius:CGFloat = 12
+    @State private var cornerRadius:CGFloat = 8
+    
+
     init(@State _ title:String ,@State _ state: String ) {
         self.statex = state
         self.title = title
@@ -17,23 +29,23 @@ struct HTextField: View {
     var body: some View {
         HStack{
             Spacer()
-            Image(systemName: "network")
+            Image(systemName: startImage)
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Spacer()
             TextField(title ,text:$statex)
-            Image(systemName: "x.circle")
+            Image(systemName: endImage)
                 .imageScale(.medium)
                 .foregroundStyle(.tint)
                 .onTapGesture {
-                    statex = ""
+                    statex = EMPTY_STRING
                 }
             Spacer()
-        }.padding(EdgeInsets(top: 4, leading: 0, bottom: 4, trailing: 0))
-        .overlay(RoundedRectangle(cornerRadius: 8)
-            .stroke(Color.purple, lineWidth: 2))
-        .cornerRadius(8)
-        .shadow(radius: 12)
+        }.padding(EdgeInsets(top: topInset, leading: leadingInset, bottom: bottomInset, trailing: trailingInset))
+        .overlay(RoundedRectangle(cornerRadius: cornerRadius)
+            .stroke(Color.purple, lineWidth: lineWidth))
+        .cornerRadius(cornerRadius)
+        .shadow(radius: shadowRadius)
         .padding()
     }
 }
