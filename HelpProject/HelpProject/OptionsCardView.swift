198aecd712c573a548e0e762c72ad10f6b894346
Pratik <kkpratik@gmail.com>
2024-07-29 20:40:49 +0530

    Create CardView

---
+++ HelpProject/HelpProject/OptionsCardView.swift
@@ -0,0 +1,18 @@
+//
+//  OptionsCardView.swift
+//  HelpProject
+//
+//  Created by Pratik Parmar on 29/07/24.
+//
+
+import SwiftUI
+
+struct OptionsCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OptionsCardView()
+}
