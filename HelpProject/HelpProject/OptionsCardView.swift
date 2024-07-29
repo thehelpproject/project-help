73b2a5cb0bd6b37faf33fdf94a134f4e3a8eddaa
Rahul Pahuja <rahulpahuja2015@gmail.com>
2024-07-29 23:05:20 +0530

    Organized OptionsCard View

--- HelpProject/HelpProject/OptionsCardView.swift
+++ HelpProject/HelpProject/OptionsCardView.swift
@@ -24,6 +24,16 @@ struct OptionsCardView: View {
     @State var cardTitle: String
     @State private var imgFillColor: Color = .purple
     
+    private var frameWidth = CGFloat(42.0)
+    private var frameHeight = CGFloat(42.0)
+    private var paddingInsetTop = CGFloat(0)
+    private var paddingInsetLeading = CGFloat(20.0)
+    private var paddingInsetBottom = CGFloat(-15.0)
+    private var paddingInsetTrailing = CGFloat(20.0)
+    private var lineWidth = CGFloat(4.0)
+    private var cornerRadius = CGFloat(12.0)
+    private var overlayCornerRadius = CGFloat(12.0)
+    
     var body: some View {
         VStack {
             Text(cardTitle)
@@ -35,8 +45,8 @@ struct OptionsCardView: View {
                     VStack {
                         Image(systemName: opt.imageName)
                             .resizable()
-                            .frame(width: 42, height:42)
-                            .padding(EdgeInsets(top: 0, leading: 20, bottom: -15, trailing: 20))
+                            .frame(width: frameWidth, height:frameHeight)
+                            .padding(EdgeInsets(top: paddingInsetTop, leading: paddingInsetLeading, bottom: paddingInsetBottom, trailing: paddingInsetTrailing))
                             .foregroundColor(imgFillColor)
                         Text(opt.displayName)
                             .padding()
@@ -45,9 +55,9 @@ struct OptionsCardView: View {
                 }
             }
         }
-        .overlay(RoundedRectangle(cornerRadius: 12)
-            .stroke(Color.purple, lineWidth: 4))
-        .cornerRadius(12)
+        .overlay(RoundedRectangle(cornerRadius: overlayCornerRadius)
+            .stroke(Color.purple, lineWidth: lineWidth))
+        .cornerRadius(cornerRadius)
         .padding()
         
     }
